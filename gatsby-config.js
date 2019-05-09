ef0cc7a0ec2d383bb924b175ac73cdbd8edf4557
Steven Chester <steven.chester@industryweapon.com>
2019-05-09 13:56:16 -0400

    gatsby config chang

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,13 @@ module.exports = {
     },
   },
   plugins: [
-  
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
